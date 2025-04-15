998858371c8af13ffccb70562a82d67a77d2f2f3
Luiz_ricci <luizaureliano.tds1.senai@gmail.com>
2025-04-15 08:37:34 -0300

    Feat: create tab navigation

---
+++ navigation/TabNavigator.js
@@ -0,0 +1,31 @@
+import React from "react"; 
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+
+const Tab = createBottomTabNavigator(); 
+
+export default function TabNavigator() {
+    return (
+        <Tab.Navigator initialRouteName="Home">
+            <Tab.Screen 
+                name="Home" 
+                component={Home} 
+                options={{
+                    tabBarIcon: ({ color, size }) => ( 
+                        <Icon name="home" color={color} size={size} /> // alterar o icon depois 
+                    ),
+                }}
+            /> 
+            <Tab.Screen 
+                name="Detalhes" 
+                component={Detalhes} 
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <AntDesign name="linechart" size={size} color={color} />   // alterar o icon depois 
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
