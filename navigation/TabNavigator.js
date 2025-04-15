93a6694e51346bd03305db896b048aa0d7698b3f
Alejandra Barros <alejandra.barros@aluno.senai.br>
2025-04-15 11:07:00 -0300

    feat: create home alejandra page

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,7 +1,8 @@
 import React from "react"; 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import AntDesign from '@expo/vector-icons/AntDesign';
+import Icon from '@expo/vector-icons/AntDesign';
 
+import HomeScreen from "../pages/HomeAlejandra";
 
 const Tab = createBottomTabNavigator(); 
 
@@ -10,19 +11,10 @@ export default function TabNavigator() {
         <Tab.Navigator initialRouteName="Home">
             <Tab.Screen 
                 name="Home" 
-                component={Home} 
+                component={HomeScreen} 
                 options={{
                     tabBarIcon: ({ color, size }) => ( 
                         <Icon name="home" color={color} size={size} /> // alterar o icon depois 
-                    ),
-                }}
-            /> 
-            <Tab.Screen 
-                name="Detalhes" 
-                component={Detalhes} 
-                options={{
-                    tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="linechart" size={size} color={color} />   // alterar o icon depois 
                     ),
                 }}
             />
