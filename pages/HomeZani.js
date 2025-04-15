37c575880abce6479a30af647ca019b4a734775e
Lucas Zani <zanin0503@gmail.com>
2025-04-15 09:01:23 -0300

    feat: add page HomeZani and PageZani

---
+++ pages/HomeZani.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, StyleSheet } from "react-native-web";
+
+export default function Home(){
+    return(
+        <View style={styles.all}>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    all: {
+        BackgroundColor: "black"
+    }
+})
