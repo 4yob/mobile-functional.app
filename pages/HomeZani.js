e81e18d43380adefb6069abbbf53002213f3c1f6
Lucas Zani <zanin0503@gmail.com>
2025-04-15 10:52:33 -0300

    feat: HomeZani structor completed

--- pages/HomeZani.js
+++ pages/HomeZani.js
@@ -1,16 +1,41 @@
 import React from "react";
-import { View, StyleSheet } from "react-native-web";
+import { View, StyleSheet } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import CardZani from "../components/CardZani";
 
-export default function Home(){
+export default function HomeZani(){
     return(
-        <View style={styles.all}>
-            
+        <View style={styles.container}>
+            <View style={styles.cards}>
+                <CardZani imagem={require("../assets/charizard.png")} text={"charizard é um pokemon zica"}/>
+                <CardZani imagem={require("../assets/gyrados.png")} text={"gyrados é um pokemon zica"} />
+                <AntDesign name="rightcircleo" size={24} color="white" />
+            </View>
+            <View style={styles.posts}>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    all: {
-        BackgroundColor: "black"
-    }
-})
+    container: {
+        backgroundColor: "white",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100%",
+        height: "100%"
+    },
+    cards: {
+        backgroundColor: "black",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "80%",
+        height: "40%",
+        borderRadius: "1rem",
+        gap: "1rem"
+    },
+    posts: {
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
